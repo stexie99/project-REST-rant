252bc82941b2e9d9e346bb0c4e8412ff2c63eb4c
Steven Xie <xies1999@hotmail.com>
2023-07-11 02:01:45 -0400

    adding 404 page route

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require('dotenv').config()
 let express = require('express')
 let app = express()
 app.get('/', (req,res)=>{
     res.send('hello world')
 })
-app.listen(3000)
+app.get('*', (req,res)=>{
+    res.statusMessage(404).send('<h1>404 Page</h1>')
+})
+app.listen(process.env.PORT)
