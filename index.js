128b0415b479cf4104fb8dbc86f8a09e51691591
Steven Xie <xies1999@hotmail.com>
2023-07-17 17:34:40 -0400

    Adding places view with mock data

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.get('/', (req,res)=>{
     res.render('home')
 })
 app.get('*', (req,res)=>{
-    res.status(404).send('<h1>404 Page</h1>')
+    res.render('error404')
 })
 app.listen(process.env.PORT)
