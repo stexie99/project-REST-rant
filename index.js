7b7e22f8f04d76204535a90b813df1a66a282a15
Steven Xie <xies1999@hotmail.com>
2023-07-18 12:25:34 -0400

    Adding styles to REST-Rant

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ require('dotenv').config()
 let express = require('express')
 let app = express()
 
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+//controllers and routes
 app.use('/places',require('./controllers/places'))
 app.get('/', (req,res)=>{
     res.render('home')
--- views/home.jsx
+++ views/home.jsx
@@ -1,11 +1,14 @@
 const React = require('react')
-const Def = require('../default')
+const Def = require('./default')
 
 function home () {
     return (
       <Def>
           <main>
               <h1>HOME</h1>
+              <a href="/places">
+                <button className="btn-primary">Places Page</button>
+              </a>
           </main>
       </Def>
     )
