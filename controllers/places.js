14602871c34521b7f2e19dac77d0c573c51409ef
Steven Xie <xies1999@hotmail.com>
2023-07-19 17:33:47 -0400

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -4,16 +4,26 @@ const places = require('../models/places.js')
 router.get('/new', (req,res)=>{
   res.render('places/new')
 })
+router.get(':/id', (req,res)=>{
+  let id = Number(req.params.id)
+  if(isNaN(id)){
+    res.render('error404')
+  }
+  else if (!places[id]){
+    res.render('error404')
+  }
+  else{
+    res.render('places/show', {places: places[id]})
+  }
+})
 router.get('/', (req,res)=>{
     let places = [{
-        key: '123',
         name: 'H-Thai-ML',
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
         pic: '/images/thai-food.jpg'
       }, {
-        key: '124',
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
